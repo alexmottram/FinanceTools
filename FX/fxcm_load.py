a05b516445a3b5b06d3dd8ef306482eb381199b4
alexmottram <alexanderdmottram@outlook.com>
2020-03-24 23:23:27 +0000

    Second commit

---
+++ FX/fxcm_load.py
@@ -0,0 +1,33 @@
+import posixpath
+import requests
+import pandas as pd
+
+BASE_DIR = '/Temp/FXData'
+
+
+def main():
+    symbol = 'GBPUSD'
+    year = 2018
+    week = 1
+    directory = posixpath.join(BASE_DIR, symbol)
+    url = f"https://tickdata.fxcorporate.com/{symbol}/{year}/{week}.csv.gz"
+    r = requests.get(url, stream=True)
+
+    filename = f"{directory}/{symbol}_{year}_w{week}.csv.gz"
+
+    with open(filename, 'wb') as file:
+        for chunk in r.iter_content(chunk_size=1024):
+            file.write(chunk)
+
+    df = pd.read_csv(
+        filename,
+        compression='gzip',
+        header=0,
+        sep=' ',
+        quotechar='"',
+        error_bad_lines=False
+    )
+
+    temp = 1
+
+
